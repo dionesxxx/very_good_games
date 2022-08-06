06f33c02232c8085c4cecda6090039a73417c1dd
Diones Camargo <dcamargo@autodoc.com.br>
2022-08-06 00:09:48 -0300

    feat: Add repository implementation

--- packages/very_good_games_repository/lib/src/very_good_games_repository.dart
+++ packages/very_good_games_repository/lib/src/very_good_games_repository.dart
@@ -1,7 +1,16 @@
+import 'package:very_good_games_api/very_good_games_api.dart';
+
 /// {@template very_good_games_repository}
 /// The interface and models for an API providing access to games
 /// {@endtemplate}
 class VeryGoodGamesRepository {
   /// {@macro very_good_games_repository}
-  const VeryGoodGamesRepository();
+  const VeryGoodGamesRepository({
+    required VeryGoodGamesApi veryGoodGamesApi,
+  }) : _veryGoodGamesApi = veryGoodGamesApi;
+
+  final VeryGoodGamesApi _veryGoodGamesApi;
+
+  ///Provide a [Stream] of all games
+  Stream<List<Game>> getGames() => _veryGoodGamesApi.getGames();
 }
--- packages/very_good_games_repository/test/src/very_good_games_repository_test.dart
+++ packages/very_good_games_repository/test/src/very_good_games_repository_test.dart
@@ -1,11 +1,66 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
+import 'package:very_good_games_api/very_good_games_api.dart';
 import 'package:very_good_games_repository/very_good_games_repository.dart';
+
+class MockVeryGoodApi extends Mock implements VeryGoodGamesApi {}
 
 void main() {
   group('VeryGoodGamesRepository', () {
-    test('can be instantiated', () {
-      expect(VeryGoodGamesRepository(), isNotNull);
+    late VeryGoodGamesApi api;
+
+    final games = [
+      Game(
+        id: 1,
+        name: 'God of War',
+        released: '-',
+        backgroundImage: 'https://',
+        rating: 4.98,
+      ),
+      Game(
+        id: 2,
+        name: 'Horizon Zero Down',
+        released: '-',
+        backgroundImage: 'https://',
+        rating: 4.92,
+      ),
+    ];
+
+    setUp(() {
+      api = MockVeryGoodApi();
+      when(
+        () => api.getGames(),
+      ).thenAnswer((_) => Stream.value(games));
+    });
+
+    VeryGoodGamesRepository createSubject() =>
+        VeryGoodGamesRepository(veryGoodGamesApi: api);
+
+    group('constructor', () {
+      test('works properly', () {
+        expect(createSubject, returnsNormally);
+      });
+    });
+
+    group('getGames', () {
+      test('makes correct api request', () {
+        final subject = createSubject();
+
+        expect(
+          subject.getGames(),
+          isNot(throwsA(anything)),
+        );
+
+        verify(() => api.getGames()).called(1);
+      });
+
+      test('returns stream of current list games', () {
+        expect(
+          createSubject().getGames(),
+          emits(games),
+        );
+      });
     });
   });
 }
